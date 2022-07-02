65e94a9a540025bdb6613f1721c3c67ff4ad0e7d
bhupin <bhupin@bottle.com.np>
2022-07-02 13:03:36 +0545

    modification in xpath

--- utils/scapper.py
+++ utils/scapper.py
@@ -32,7 +32,7 @@ class DataScrapper:
 		WebDriverWait(self.driver, 20).until(
 			EC.presence_of_element_located((By.XPATH, "//input[@id='fromdate']"))
 		)
-		date_input = self.driver.find_element_by_xpath("//input[@id='fromdate']")
+		date_input = self.driver.find_element("xpath", "//input[@id='fromdate']")
 		search_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='fromdate']")))
 		date_input.send_keys(self.date)
 		search_btn.click()
